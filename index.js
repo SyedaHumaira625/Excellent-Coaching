c95d36335ae1e484014ac889d6937e6ed2f2c476
SyedaHumaira625 <syedahumaira399@gmail.com>
2025-05-15 04:35:14 -0700

    header created

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ let loader = document.querySelector(".loader");
 setTimeout(function () {
   loader.style.top = "-100%";
 }, 3000);
+
+let navBar = document.querySelector("nav");
+let iconXmark = document.querySelector("#cross-icon");
+let iconMenu = document.querySelector("#menu-icon");
+
+iconXmark.addEventListener("click", function () {
+  navBar.style.transform = "translateY(-100%)";
+});
+
+
